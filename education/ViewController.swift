f377540830feec2d16771904c46486815307c246
Yuriy T <xeon@Mac-mini-Yuriy.local>
2017-04-21 10:29:44 +0300

    init commit

---
+++ education/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  education
+//
+//  Created by Yuriy T on 21.04.17.
+//  Copyright © 2017 education.t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
